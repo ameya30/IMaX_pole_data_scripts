41931925e66182b6e6aebe264e67d45b19302ca9
ameya30 <ameyaprabhu30@gmail.com>
2017-10-20 09:50:39 +0200

    changes made in view_cycles to display fits files for animation

--- my_scripts/view_cycles.py
+++ my_scripts/view_cycles.py
@@ -7,20 +7,29 @@ import matplotlib.animation as animation
 
 import numpy as np
 from scipy.io import readsav
+from astropy.io import fits
 from natsort import natsorted
 
-#for workstation
-#files = natsorted(glob.glob('/home/prabhu/sunrise_holly/movie_data/*.sav'))
+########for workstation
+files = natsorted(glob.glob('/home/prabhu/sunrise_holly/movie_data/*.sav')) #to read in my restored .sav files from mk_magneto_restor
 
-#for pulpo
-#mine
-files = natsorted(glob.glob('/scratch/prabhu/HollyWaller/IMaX_pole_data_scripts/primary_scripts/saves_Oct11/mk_magneto_tr2*'))
-#holly
+
+
+#### to read in .fits from holly's post demod fringe removal
+# files = natsorted(glob.glob('/home/prabhu/sunrise_holly/IMaX_pole_data_scripts/post_demod_output_/*.fits')) 
+# data = [fits.open(i)[0].data for i in files]
+
+
+########for pulpo
+#####mine
+# files = natsorted(glob.glob('/scratch/prabhu/HollyWaller/IMaX_pole_data_scripts/primary_scripts/saves_Oct11/mk_magneto_tr2*'))
+####holly
 #files = natsorted(glob.glob('/scratch/prabhu/HollyWaller/IMaX_pole_data_scripts/mk_magneto_outputreduc_rnr_/*.sav'))
 
-files = list(filter(lambda x: x[-3:-1]=='sa',files))
 
+files = list(filter(lambda x: x[-3:-1]=='sa',files))
 data = [readsav(i,python_dict = True)['iid'] for i in files]
+
 
 lam = 4
 pol = 0
@@ -31,11 +40,7 @@ print (data[7][pol,lam,:,:].min())
 
 fig = plt.figure(figsize=(12,12))
 ax = plt.axes()
-<<<<<<< HEAD
-im = ax.imshow(data[0][pol,lam,:,:],cmap='gray',vmax=8250,vmin=75)
-=======
-im = ax.imshow(data[0][pol,lam,:,:],cmap='gray',vmax=8300,vmin=100)
->>>>>>> 80d114b750c0844c3e26ea77c4e8709ded830279
+im = ax.imshow(data[0][pol,lam,:,:],cmap='gray',vmax=6550,vmin=50)
 fig.colorbar(im)
 fig.tight_layout(pad=1.8)
 plt.gca().invert_yaxis()
@@ -44,9 +49,6 @@ def init():
 	im.set_data(np.ma.array(data[0][3,2,:,:]))#,mask=True),cmap='gray')
 	return im,
 
-# im = plt.imshow(data[0][3,2,:,:],cmap='gray',vmax=340,vmin=-140)
-
-# im = plt.imshow(np.ma.array(data[0][3,2,:,:],mask=True),cmap='gray')
 
 def animate(i):
 	im.set_data(data[i][pol,lam,:,:])#,cmap='gray',vmax=340,vmin=-140)
@@ -54,9 +56,10 @@ def animate(i):
 	return im,
 
 
-# ani = animation.FuncAnimation(fig,animate,np.arange(len(data)),init_func = init,interval = 300,blit = True)
 
 ani = animation.FuncAnimation(fig,animate,np.arange(len(data)),interval = 1000,blit = False)
+
+ani
 
 plt.show()
 
